26ec8da24bcb8237a892e7c2a344754d84770c2f
Jemagif78 <ermekgd2004@gmail.com>
2023-06-26 01:52:16 +0600

    update2
    flush

--- script/playaudio.js
+++
@@ -1,9 +0,0 @@
-function audioWARN() {
-    let audio = document.getElementById("audioWARN01");
-    audio.play();
-}
-
-function audioERR() {
-    let audio = document.getElementById("audioERR01");
-    audio.play();
-}
